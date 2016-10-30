4d6cadb8a90a625ffdfe0c0255a4dfdf0d353caa
rajesh372 <rxt160630@utdallas.edu>
2016-10-29 23:44:51 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-export class BasicChart {
-  constructor(data) {
-    var d3 = require('d3'); // Require D3 via Webpack
-    this.data = data;
-    this.svg = d3.select('div#chart').append('svg');
-  }
-}
-var chart = new BasicChart();
+
